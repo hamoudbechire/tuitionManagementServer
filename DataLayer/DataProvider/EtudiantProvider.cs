27552bc518589e0bc9e0e95fc90910685c301ca3
sidelmoctar <sidelmokhtar.sidimohamed@gmail.com>
2022-03-29 15:31:50 +0000

    no modif

--- DataLayer/DataProvider/EtudiantProvider.cs
+++ DataLayer/DataProvider/EtudiantProvider.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataLayer.DataProvider
+namespace DataLayer
 {
     class EtudiantProvider
     {
-
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
     }
 }
