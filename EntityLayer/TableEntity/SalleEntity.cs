e980355f00d9fae06fcdc6ccbd86ac5c1aafbc08
= <=>
2022-03-29 13:34:36 +0000

    Edit entity salle and admin

--- EntityLayer/TableEntity/SalleEntity.cs
+++ EntityLayer/TableEntity/SalleEntity.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EntityLayer.TableEntity
+namespace EntityLayer
 {
     [Table("Salle")]
     public class SalleEntity
