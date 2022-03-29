06f5c973d3439a91419cf3919e9607eedc10308a
sidelmoctar <sidelmokhtar.sidimohamed@gmail.com>
2022-03-29 13:21:43 +0000

    add student entity and class entity

---
+++ EntityLayer/TableEntity/ClasseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.TableEntity
+{
+    [Table("etudiant")]
+    public class ClasseEntity
+    {
+
+        [Key]
+        [JsonProperty("classId")]
+        public int ClasseId { get; set; }
+        [JsonProperty("className")]
+        public string ClassName { get; set; }
+        public List<EtudiantEntity> etudiants { get; set; }
+        public List<ProffesseurEntity> proffesseurs { get; set; }
+
+
+    }
+}
---
+++ EntityLayer/TableEntity/EtudiantEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.TableEntity
+{
+    [Table("etudiant")]
+    public class EtudiantEntity
+    {
+        [Key]
+        [JsonProperty("etudiantId")]
+        public int EtudiantId { get; set; }
+        [JsonProperty("firstname")]
+        public string FirstName { get; set; }
+        [JsonProperty("lastname")]
+        public string LastName { get; set; }
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+        [JsonProperty("classes")]
+        public List<ClasseEntity> Classes { get; set; }
+
+    }
+}
