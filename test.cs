2db8f7d6f38383fc6e59ca22b76480fffa5297b5
Aynin <amelayni@gmail.com>
2022-03-29 11:38:00 +0200

    commit test

---
+++ test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagementApi
+{
+    public class test
+    {
+        public int a;
+    }
+}
