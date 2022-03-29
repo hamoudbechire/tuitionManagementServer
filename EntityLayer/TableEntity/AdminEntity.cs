61d5d33d35d0d9ea261e0a51c1863993c59cea5c
= <=>
2022-03-29 11:04:49 +0100

    test add folder

---
+++ EntityLayer/TableEntity/AdminEntity.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.TableEntity
+{
+    [Table("Admin")]
+    public class AdminEntity
+    {
+        [Key]
+        [JsonProperty("adminId")]
+        public int Id;
+        [JsonProperty("firstName")]
+        public string FirstName;
+
+    }
+}
