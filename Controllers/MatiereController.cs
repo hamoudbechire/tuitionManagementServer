863f8d57f8e80bc4613a175acab29c4023df0e76
Aynin <amelayni@gmail.com>
2022-03-29 19:45:00 +0200

    Controllers Proffesseur et Matiere

--- Controllers/MatiereController.cs
+++ Controllers/MatiereController.cs
@@ -1,11 +1,59 @@
-﻿using System;
+﻿using BusinessLayer;
+using EntityLayer.TableEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
+//using System.Web.Mvc;
 
 namespace SchoolManagementApi.Controllers
 {
-    public class MatiereController
+    public class MatiereController : ApiController
     {
+        // GET: All Matiere
+        public List<MatiereEntity> Get()
+        {
+            return ManageMatiere.ListMatiere(obj => true);
+        }
+        // GET: Matiere by id
+        public MatiereEntity Get(int id)
+        {
+            return ManageMatiere.ListMatiere(obj => obj.IdMatiere == id).FirstOrDefault();
+        }
+        // Post Matiere
+        public bool Post(MatiereEntity matiere)
+        {
+            var result = false;
+            if(matiere != null)
+            {
+                if (matiere.IdMatiere > 0)
+                {
+                    result = ManageMatiere.ModifyMatiere(matiere);
+                }
+                else
+                {
+                    result = ManageMatiere.NewMatiere(matiere);
+                }
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        // Delete Matiere By Id
+        public bool Delete(int id)
+        {
+            var result = false;
+            var matiere = Get(id);
+
+            if(matiere != null)
+            {
+                result = ManageMatiere.DeleteMatiere(matiere);
+            }
+            return result;
+        }
     }
 }
---
+++ Controllers/ProffesseurController.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer;
+using EntityLayer.TableEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SchoolManagementApi.Controllers
+{
+    public class ProffesseurController : ApiController
+    {
+        //[Authorize]
+        //[Route("api/proffesseur")]
+        //[AcceptVerbs("GET")]
+        // GET: Proffesseur
+         
+        [AcceptVerbs("GET")]
+        public List<ProffesseurEntity> GET()
+        {
+            var profs = ManageProffesseur.ListProf(obj => true);
+            return profs;
+        }
+        public ProffesseurEntity GET(int id)
+        {
+            var prof = ManageProffesseur.ListProf(obj => obj.ProfId == id).FirstOrDefault();
+            return prof;
+        }
+        [AcceptVerbs("POST")]
+        public bool POST(ProffesseurEntity prof)
+        {
+            var result = false;
+            if(prof != null) {
+                if (prof.ProfId > 0)
+                {
+                    result = ManageProffesseur.ModifyProf(prof);
+                }
+                else
+                {
+                    result = ManageProffesseur.NewProf(prof);
+                }
+            } 
+                
+            return result;
+        }
+    }
+}
