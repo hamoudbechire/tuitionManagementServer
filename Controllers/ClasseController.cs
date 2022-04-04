47aca9e66a387d8bd326aa530f79df1a9fc1f17e
sidelmoctar <74023686+sidelmoctar@users.noreply.github.com>
2022-04-04 07:39:51 -0700

    update classe controller

--- Controllers/ClasseController.cs
+++ Controllers/ClasseController.cs
@@ -1,4 +1,4 @@
-﻿using BusinessLayer;
+using BusinessLayer;
 using EntityLayer.TableEntity;
 using System;
 using System.Collections.Generic;
@@ -12,25 +12,34 @@ namespace SchoolManagementApi.Controllers
     public class ClasseController : ApiController
     {
         [AllowAnonymous]
-        //[Route("api/etudiant/post")]
-        public bool AjouterClasse(ClasseEntity classe)
+        [AcceptVerbs("POST")]
+        [Route("api/classe/post")]
+        public bool AddNewClasse([FromBody]  ClasseEntity classe)
         {
             return ManageClasse.NewClasse(classe);
         }
         [AllowAnonymous]
-       // [Route("api/etudiant/get/id?")]
-        public List<ClasseEntity> GetById(int id)
+        [AcceptVerbs("GET")]
+        [Route("api/classe/list")]
+        public List<ClasseEntity> GetAllClasse()
+        {
+            var classes = ManageClasse.ListClasse(obj => true);
+            return classes;
+        }
+        [AllowAnonymous]
+        [Route("api/classe/get/{id}")]
+        public List<ClasseEntity> GetClasseById(int id)
         {
             return ManageClasse.ListClasse(t => (t.Id == id));
         }
-        //[Route("api/etudiant/put/id?")]
-        public void EditerClasse([FromBody] ClasseEntity classe)
+        [Route("api/classe/put/{id}")]
+        public void EditClasse([FromBody] ClasseEntity classe)
         {
             ManageClasse.UpdateClasse(classe);
         }
 
-        //[Route("api/etudiant/delete/id?")]
-        public void SupprimerClasse(int id)
+        [Route("api/classe/delete/{id}")]
+        public void DeleteClasse(int id)
         {
             var item = ManageClasse.ListClasse(i => i.Id == id).FirstOrDefault();
             if (item != null)
