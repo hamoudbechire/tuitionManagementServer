c321019a02bd6cef04c39584f932bddff4354479
Hadrami <abdrahmane201@gmail.com>
2022-04-06 12:17:47 +0000

    update api admin

---
+++ SimpleRefreshTokenProvider.cs
@@ -0,0 +1,106 @@
+﻿using EntityLayer;
+using BusinessLayer;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Infrastructure;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SchoolManagementApi.Providers
+{
+    public class SimpleRefreshTokenProvider : IAuthenticationTokenProvider
+    {
+
+        public async Task CreateAsync(AuthenticationTokenCreateContext context)
+        {
+            var clientid = context.Ticket.Properties.Dictionary["as:client_id"];
+
+            if (string.IsNullOrEmpty(clientid))
+            {
+                return;
+            }
+
+            var refreshTokenId = Guid.NewGuid().ToString("n");
+
+            using (AuthRepository _repo = new AuthRepository())
+            {
+                var refreshTokenLifeTime = context.OwinContext.Get<string>("as:clientRefreshTokenLifeTime");
+                var token = new RefreshTokenEntity();
+                if (context.Ticket.Identity.Name.Contains("@gmail.com"))
+                {
+
+                    AdminEntity user = ManageAdmin.ListAdmin(u => u.Email == context.Ticket.Identity.Name).FirstOrDefault();
+
+
+                    token.Id = Helper.GetHash(refreshTokenId);
+                    token.ClientId = clientid;
+                    token.Subject = user.Phone;
+                    token.IssuedUtc = DateTime.UtcNow;
+                    token.ExpiresUtc = DateTime.UtcNow.AddMinutes(Convert.ToDouble(refreshTokenLifeTime));
+                }
+                else
+                {
+
+
+                    token.Id = Helper.GetHash(refreshTokenId);
+                    token.ClientId = clientid;
+                    token.Subject = context.Ticket.Identity.Name;
+                    token.IssuedUtc = DateTime.UtcNow;
+                    token.ExpiresUtc = DateTime.UtcNow.AddMinutes(Convert.ToDouble(refreshTokenLifeTime));
+
+
+                }
+
+
+
+
+                context.Ticket.Properties.IssuedUtc = token.IssuedUtc;
+                context.Ticket.Properties.ExpiresUtc = token.ExpiresUtc;
+
+                token.ProtectedTicket = context.SerializeTicket();
+
+                var result = await _repo.AddRefreshToken(token);
+
+                if (result)
+                {
+                    context.SetToken(refreshTokenId);
+                }
+
+            }
+        }
+
+        public async Task ReceiveAsync(AuthenticationTokenReceiveContext context)
+        {
+
+            var allowedOrigin = context.OwinContext.Get<string>("as:clientAllowedOrigin");
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { allowedOrigin });
+
+            string hashedTokenId = Helper.GetHash(context.Token);
+
+            using (AuthRepository _repo = new AuthRepository())
+            {
+                var refreshToken = await _repo.FindRefreshToken(hashedTokenId);
+
+                if (refreshToken != null)
+                {
+                    //Get protectedTicket from refreshToken class
+                    context.DeserializeTicket(refreshToken.ProtectedTicket);
+                    var result = await _repo.RemoveRefreshToken(hashedTokenId);
+                }
+            }
+        }
+
+        public void Create(AuthenticationTokenCreateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Receive(AuthenticationTokenReceiveContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
